30f2febd8996a4370ac2231cbdc10430ec9bef20
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-11-13 22:02:47 +0300

    Update ApiError.java

--- ewm-service/src/main/java/ru/practicum/explorewithme/common/ApiError.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/common/ApiError.java
@@ -1,11 +1,13 @@
 package ru.practicum.explorewithme.common;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class ApiError {
     private List<String> error;
