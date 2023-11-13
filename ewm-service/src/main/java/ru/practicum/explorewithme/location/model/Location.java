c4ce50523da9854bc0fc361727fd6178a921f3c6
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-11-13 23:54:05 +0300

    Update Location.java

--- ewm-service/src/main/java/ru/practicum/explorewithme/location/model/Location.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/location/model/Location.java
@@ -2,15 +2,21 @@ package ru.practicum.explorewithme.location.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity(name = "locations")
-@Data
+@Getter
+@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class Location {
 
