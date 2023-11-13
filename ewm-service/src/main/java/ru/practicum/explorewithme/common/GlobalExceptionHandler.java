5b122033d78cda0c1982355596fe3d35da24d657
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-11-13 23:49:27 +0300

    Update GlobalExceptionHandler.java

--- ewm-service/src/main/java/ru/practicum/explorewithme/common/GlobalExceptionHandler.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/common/GlobalExceptionHandler.java
@@ -31,215 +31,68 @@ public class GlobalExceptionHandler {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    @ExceptionHandler
+    @ExceptionHandler({RuntimeException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiError handleRuntimeException(final RuntimeException e) {
+    public ApiError handleException(Exception e) {
         StackTraceElement[] stackTrace = e.getStackTrace();
         List<String> errors = new ArrayList<>();
         for (StackTraceElement stackTraceElement : stackTrace) {
             errors.add(stackTraceElement + "\n");
         }
-
         log.info(errors + "MESSAGE: " + e.getMessage());
 
-        return new ApiError(errors, e.getMessage(), "Runtime exception",
+        return new ApiError(errors, e.getMessage(), e.getLocalizedMessage(),HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                LocalDateTime.now().format(FORMATTER));
+    }
+
+    @ExceptionHandler({FieldIsNotValidException.class, EventBadTimeException.class, MethodArgumentNotValidException.class, LocationsFieldsIsEmptyException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleRuntimeException(Exception e) {
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        List<String> errors = new ArrayList<>();
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            errors.add(stackTraceElement + "\n");
+        }
+        log.info(errors + "MESSAGE: " + e.getMessage());
+
+        return new ApiError(errors, e.getMessage(), e.getLocalizedMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.toString(), LocalDateTime.now().format(FORMATTER));
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        log.info(errors + "MESSAGE: " + e.getMessage());
-
-        return new ApiError(errors, e.getMessage(), "Not valid field",
-                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleFieldIsNotValidException(final FieldIsNotValidException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Field is not valid",
-                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleLocationsFieldsIsEmptyException(final LocationsFieldsIsEmptyException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Field is not valid",
-                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
+    @ExceptionHandler({EventBadStateException.class,
+            ParticipantLimitException.class,
+            DuplicateRequestException.class,
+            RequestOwnerException.class,
+            EventIsNotPublishedException.class,
+            DataIntegrityViolationException.class
+    })
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleEventBadStateException(final EventBadStateException e) {
+    public ApiError handleEventBadStateException(Exception e) {
         StackTraceElement[] stackTrace = e.getStackTrace();
         List<String> errors = new ArrayList<>();
         for (StackTraceElement stackTraceElement : stackTrace) {
             errors.add(stackTraceElement + "\n");
         }
 
-        return new ApiError(errors, e.getMessage(), "Inappropriate status",
+        return new ApiError(errors, e.getMessage(), e.getLocalizedMessage(),
                 HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
     }
 
-    @ExceptionHandler
+    @ExceptionHandler({CategoryNotFoundException.class,
+            CompilationNotFoundException.class,
+            UserNotFoundException.class,
+            EventNotFoundException.class,
+            LocationNotFoundException.class
+    })
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleEventNotFoundException(final EventNotFoundException e) {
+    public ApiError handleEventNotFoundException(Exception e) {
         StackTraceElement[] stackTrace = e.getStackTrace();
         List<String> errors = new ArrayList<>();
         for (StackTraceElement stackTraceElement : stackTrace) {
             errors.add(stackTraceElement + "\n");
         }
 
-        return new ApiError(errors, e.getMessage(), "The required object was not found",
+        return new ApiError(errors, e.getMessage(), e.getLocalizedMessage(),
                 HttpStatus.NOT_FOUND.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleUserNotFoundException(final UserNotFoundException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "The required object was not found",
-                HttpStatus.NOT_FOUND.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleCompilationNotFoundException(final CompilationNotFoundException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "The required object was not found",
-                HttpStatus.NOT_FOUND.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleCategoryNotFoundException(final CategoryNotFoundException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "The required object was not found",
-                HttpStatus.NOT_FOUND.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleLocationNotFoundException(final LocationNotFoundException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "The required object was not found",
-                HttpStatus.NOT_FOUND.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleParticipantLimitException(final ParticipantLimitException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Request limit exceeded",
-                HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleDuplicateRequestException(final DuplicateRequestException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Repeated request",
-                HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleUserIsOwnerEventException(final RequestOwnerException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Wrong user",
-                HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleEventIsNotPublishedException(final EventIsNotPublishedException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Only published events can be changed",
-                HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleDataException(final DataIntegrityViolationException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Database error",
-                HttpStatus.CONFLICT.toString(), LocalDateTime.now().format(FORMATTER));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleEventBadTimeException(final EventBadTimeException e) {
-        StackTraceElement[] stackTrace = e.getStackTrace();
-        List<String> errors = new ArrayList<>();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            errors.add(stackTraceElement + "\n");
-        }
-
-        return new ApiError(errors, e.getMessage(), "Field 'time' is not valid",
-                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now().format(FORMATTER));
     }
 }
