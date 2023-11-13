75effa35ed99b77cf775e1131efbdb5b295ed90b
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-11-14 00:12:20 +0300

    Update User.java

--- ewm-service/src/main/java/ru/practicum/explorewithme/user/model/User.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/user/model/User.java
@@ -1,10 +1,21 @@
 package ru.practicum.explorewithme.user.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Data
+
+@Getter
+@Setter
 @Entity(name = "users")
 @Builder
 @EqualsAndHashCode(of = "id")
